56ac6f4e2de864b1a0fe042ad519ed1fa61afeb4
Chandramauli <chandramauliguptach@gmail.com>
2024-01-03 18:50:28 +0530

    added function to save notes to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { createStore } from "redux";
 import reducers  from "./reducers";
+import { loadState, saveState } from './middleware/localStorage.js';
 
+const persistedState = loadState();
 
 const store = createStore(
     reducers,
-    {},
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+
+store.subscribe(() => {
+    saveState(store.getState());
+  });  
 
 export default store;
